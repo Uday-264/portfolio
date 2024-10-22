172c9eeeeaaad936f6606d959d0bd1c5a67fbb90
Uday-264 <Uday-264@users.noreply.github.com>
2024-10-22 10:01:16 +0000

    deploy: f05a0d412f6c146cb2e23584b3bb34e8b400eea7

---
+++ assets/chunk-B2MGPQRJ-c6163b4f.js
@@ -0,0 +1,1 @@
+import{f as i,u as t,o as c,j as r,b as o,c as m}from"./index-e80d72a1.js";var d=i(function(a,s){const e=t("Heading",a),{className:f,...n}=c(a);return r.jsx(o.h2,{ref:s,className:m("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
